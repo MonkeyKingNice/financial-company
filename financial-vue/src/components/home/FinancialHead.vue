fadebf934eb77a1580bb0336f54e555852c2c111
zzl <15994794487@163.com>
2020-05-26 22:08:09 +0800

    新加基本组件

--- financial-vue/src/components/home/FinancialHead.vue
+++ financial-vue/src/components/home/FinancialHead.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <li>
-      众所周知
-    </li>
+    <li>众所周知</li>
     <li>网站首页</li>
     <li>一站式服务</li>
     <li>公司理念</li>
@@ -10,3 +8,21 @@
     <li>联系我们</li>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'FinancialHead',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+
+  },
+
+  computed: {},
+
+  methods: {}
+}
+</script>
